12fb74cc349085b8fbd9dfbae9626a50a9d9f51f
Michael Cieplak <mcieplak@yahoo-inc.com>
2017-01-09 19:28:25 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Michael Cieplak on 1/9/17.
+//  Copyright © 2017 Michael Cieplak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
